8221fc24a66427492a86625da42a850b30d1d006
Leo <leolmq#gmail.com>
2016-02-25 18:12:40 +0800

    move player
    move player and in limit boundary

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 10f;
+	public Boundary boundary;
+	public float tilt = 4f;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical);
+		this.rigidbody.velocity = movement * speed;
+
+		this.rigidbody.position = new Vector3
+		(
+			Mathf.Clamp(this.rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0f,
+			Mathf.Clamp(this.rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		this.rigidbody.rotation = Quaternion.Euler (0f, 0f, this.rigidbody.velocity.x * tilt);
+	}
+}
