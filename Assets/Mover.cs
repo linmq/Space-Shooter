1ddfbef2de842041edc4ba79ced73702838ba343
Leo <leolmq#gmail.com>
2016-02-25 18:32:38 +0800

    add bolt
    add bolt

---
+++ Assets/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed = 20f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.rigidbody.velocity = this.transform.forward * speed;
+	}
+}
