f6130cc78ddeea45c368d7aee7e7f19912115ee4
Leo <leolmq#gmail.com>
2016-02-26 14:08:32 +0800

    add asteroid explosion
    addn asteroid and player explosion, move asteroid

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour 
 {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Boundary")
 		{
 			return;
 		}
+		Instantiate (explosion, this.transform.position, this.transform.rotation);
 		Destroy (this.gameObject);
+
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
 		Destroy (other.gameObject);
 	}
 }
