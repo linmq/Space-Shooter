45820c2e86c2c50a1c4e7fa44b8dd268bd49be4b
Leo <leolmq#gmail.com>
2016-02-26 14:42:59 +0800

    create waves asteroid
    create waves asteroid; destroy explosion VFX

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	public int hazardCount = 10;
+	public float spawnWait = .5f; 
+	public float waveWait = 4f;
+	public float startWait = 1f;
+
+	void Start()
+	{
+		StartCoroutine (SpawnWaves());
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds (startWait);
+		while(true)
+		{
+			for (int i=0; i<hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+}
