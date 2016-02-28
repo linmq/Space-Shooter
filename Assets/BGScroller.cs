e3823d4f1481985ef8175b4b7a3147d981e9cf58
linmq <leolmq@gmail.com>
2016-02-28 11:56:23 +0800

    background scroll
    background scroll

---
+++ Assets/BGScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float tileSizeZ;
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		this.transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
